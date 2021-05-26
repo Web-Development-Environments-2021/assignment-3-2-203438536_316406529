12a1395e68d386780290d09e92df59211b29bb30
noam <kotlar@post.bgu.ac.il>
2021-05-26 11:54:03 +0300

    init favorites teams update

---
+++ project/routes/favorites.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+const DButils = require("./utils/DButils");
+const players_utils = require("./utils/favorites_utils");
+
+router.get("/teamFullDetails/:teamId", async (req, res, next) => {
+    let team_players = [];
+    let team_coach = "";
+    try {
+      const team_players = await players_utils.getPlayersByTeam(
+        req.params.teamId
+      );
+      const team_coach = await players_utils.getCoachNameByTeam(
+        req.params.teamId
+      );
+      //we should keep implementing team page.....
+      res.send({team_players: team_players,team_coach: team_coach});
+    } catch (error) {
+      next(error);
+    }
+  });
+
+module.exports = router;
