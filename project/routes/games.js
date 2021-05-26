2dce4e1b052c308a738cce9786c0c3d5e2861140
aviranGit <avirangi@post.bgu.ac.il>
2021-05-26 23:47:13 +0300

    add game fix

--- project/routes/games.js
+++ project/routes/games.js
@@ -2,12 +2,11 @@ var express = require("express");
 var router = express.Router();
 const gamees_utils = require("./utils/games_utils");
 
-router.post("/LeagueManagment/Games", async (req, res, next) => {
+router.post("/LeagueManagment/addGame", async (req, res, next) => {
     try{
-        data = req.session;
+        data = await req.body;
         await gamees_utils.AddGame(data);
         res.status(201).send("The game have been added successfully");
-
     }catch(error){
         next(error);
     }
