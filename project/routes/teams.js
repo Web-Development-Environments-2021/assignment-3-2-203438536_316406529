101e80ca3eee24e0221fff35bedaf50eb45239f5
aviranGit <avirangi@post.bgu.ac.il>
2021-06-04 15:55:25 +0300

    fix leauge data

--- project/routes/teams.js
+++ project/routes/teams.js
@@ -8,36 +8,35 @@ router.get("/teamFullDetails/:teamId", async (req, res, next) => {
   let team_players = [];
   let team_coach = "";
   try {
-    const checkTeamLeague = teams_utils.checkTeamLeague(req.params.teamId);
-    if(!checkTeamLeague){
+    const checkTeamLeague = await teams_utils.checkTeamLeague(
+      req.params.teamId
+    );
+    if (!checkTeamLeague) {
       res.send("Team not in league 271");
     }
-    const team_players = await teams_utils.getPlayersByTeam(
-      req.params.teamId
-    );
-    const team_coach = await teams_utils.getCoachNameByTeam(
-      req.params.teamId
-    );
+    const team_players = await teams_utils.getPlayersByTeam(req.params.teamId);
+    const team_coach = await teams_utils.getCoachNameByTeam(req.params.teamId);
     const team_games_upcoming = await teams_utils.getUpcomingTeamGames(
       req.params.teamId
     );
-    res.send({team_players: team_players,team_coach: team_coach,team_games_upcoming: team_games_upcoming});
+    res.send({
+      team_players: team_players,
+      team_coach: team_coach,
+      team_games_upcoming: team_games_upcoming,
+    });
   } catch (error) {
     next(error);
   }
 });
 
 router.get("/search/:searchKey", async (req, res, next) => {
-  try{
+  try {
     const search_key = req.params.searchKey;
-    const team_details = await teams_utils.getTeamByName(
-      search_key
-    );
+    const team_details = await teams_utils.getTeamByName(search_key);
     res.send(team_details);
   } catch (error) {
     next(error);
   }
 });
-
 
 module.exports = router;
--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -11,35 +11,47 @@ async function getLeagueData() {
       api_token: process.env.api_token,
     },
   });
-  const stage = await axios.get(
-    `${api_domain}/stages/${league.data.data.current_stage_id}`,
-    {
-      params: {
-        api_token: process.env.api_token,
-      },
-    }
-  );
+  let stage;
+  if (league.data.data.current_stage_id) {
+    stage = await axios.get(
+      `${api_domain}/stages/${league.data.data.current_stage_id}`,
+      {
+        params: {
+          api_token: process.env.api_token,
+        },
+      }
+    );
+    stage = stage.data.data.name;
+  } else {
+    stage = "Not Exist";
+  }
   let currentDate = new Date().toISOString();
   //need to fix this query
   const nextGame =
     await DButils.execQuery(`select top 1 game_date, game_hour, home_team, away_team, field \
   from dbo.games WHERE game_date >= '${currentDate}'  ORDER BY game_date ASC, game_hour ASC`);
-  const { game_hour, game_date, home_team, away_team, field } = nextGame[0];
-  let game_hour_split = String(game_hour).slice(16, 25);
-  let game_date_split = String(game_date).slice(0, 15);
+  let nextGameInfo;
+  if (nextGame[0]) {
+    const { game_hour, game_date, home_team, away_team, field } = nextGame[0];
 
-  return {
-    league_name: league.data.data.name,
-    current_season_name: league.data.data.season.data.name,
-    current_stage_name: stage.data.data.name,
-    // next game details should come from DB
-    nextComingGame: {
+    let game_hour_split = String(game_hour).slice(16, 25);
+    let game_date_split = String(game_date).slice(0, 15);
+    nextGameInfo = {
       game_date: game_date_split,
       game_hour: game_hour_split,
       home_team: home_team,
       away_team: away_team,
       field: field,
-    },
+    };
+  } else {
+    nextGameInfo = "Not Exist";
+  }
+  return {
+    league_name: league.data.data.name,
+    current_season_name: league.data.data.season.data.name,
+    current_stage_name: stage,
+    // next game details should come from DB
+    nextComingGame: nextGameInfo,
   };
 }
 
